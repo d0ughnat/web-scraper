ca5fc9fb2fe611ca92982eebb1a1e09b80358574
Prince Sanguan <princesanguan44@gmail.com>
2025-02-27 21:24:12 +0800

    Update CORS settings and add Vercel configuration for API headers

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = FastAPI()
 # Enable CORS to allow React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:8000", "https://choros.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
